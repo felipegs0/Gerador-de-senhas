13e7fc214052efbafb587771f1f2799be7c63734
Felipe Gomes <felipegstech@gmail.com>
2025-06-04 19:46:07 -0300

    uptade

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,9 @@ const copy = document.querySelector('.inCopy')
 
 const submit = document.getElementById('inEnviar') // remover caso não seja usado
 
-let randomizados = 27
-
 function random() {
-    return Math.floor(Math.random() * randomizados)
+    return Math.floor(Math.random() * gerador.length)
 }
-
-document.addEventListener('click', (e) => {
-    const target = e.target
-    console.log(target)
-    if(upper.checked) {
-        console.log('oii')
-    } else {
-        console.log('nao')
-    }
-})
 
 let gerador = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g',
@@ -33,7 +21,7 @@ let gerador = [
     'v', 'w', 'x', 'y', 'z'
 ];
 
-const maiusculo = () => {
+const upperF = () => {
     const alfabetoMaiusculo = [
         'A', 'B', 'C', 'D', 'E', 'F', 'G',
         'H', 'I', 'J', 'K', 'L', 'M', 'N',
@@ -44,18 +32,16 @@ const maiusculo = () => {
     for (let i = 0; i < alfabetoMaiusculo.length; i++) {
         gerador.push(alfabetoMaiusculo[i])
     }
-    randomizados += 27;
 }
 
-const numeros = () => {
+const numberF = () => {
     const numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     for (let i = 0; i < numeros.length; i++) {
         gerador.push(numeros[i])
     }
-    randomizados += 11;
 }
 
-const simbolos = () => {
+const symbolF = () => {
     const simbolos = [
         '!', '@', '#', '$', '%', '&', '*',
         '-', '_', '+', '=', '.', '?'
@@ -63,35 +49,45 @@ const simbolos = () => {
     for (let i = 0; i < simbolos.length; i++) {
         gerador.push(simbolos[i]);
     };
-    randomizados += 14;
 }
+
+document.addEventListener('click', (e) => {
+    const target = e.target
+    
+    if(target == upper) {
+        if(upper.checked) {
+        upperF()
+    } else {
+        gerador = gerador.filter(noUpp => !/^[A-Z]$/.test(noUpp))
+    }
+    }
+
+    if(target == number) {
+        if(number.checked) {
+        numberF();
+    } else {
+        gerador = gerador.filter(noNum => !/[0-9]/.test(noNum))
+    }
+    }
+
+    if(target == symbol) {
+        if(symbol.checked) {
+        symbolF();
+    } else {
+        gerador = gerador.filter(noSym => !/[!@#$%&*-_+=.?]/.test(noSym))
+    }
+    }
+})
 
 let senhaBruta = []
 
 frm.addEventListener('submit', (e) => {
     e.preventDefault()
-
-    if(upper.checked) {
-        maiusculo();
-    } else {
-        gerador = gerador.filter(semMaiuscula => !/^[A-Z]$/.test(semMaiuscula))
-        
-    }
-
-    if(number.checked) {
-        numeros();
-    }
-
-    if(symbol.checked) {
-        simbolos();
-    }
-
     senhaBruta = []
     for(let i = 0; i < range.value ; i++) {
         senhaBruta.push([...gerador][random()])
     }
 
-    console.log(gerador)
     display.innerText = (senhaBruta.join(''))
 })
 
@@ -102,4 +98,6 @@ range.addEventListener('change', () => {
 
 copy.addEventListener('click', () => {
     navigator.clipboard.writeText(display.innerText)
+
+    console.log('Senha copiada com sucesso')
 })
